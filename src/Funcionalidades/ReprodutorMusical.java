0ba915e326b5e5ff374d9e760c9b94ff97246f4d
Dann-09 <dn.oliv@outlook.com>
2024-09-26 13:39:25 -0300

    Iphone-Reprodutor-Pronto

---
+++ src/Funcionalidades/ReprodutorMusical.java
@@ -0,0 +1,39 @@
+package Funcionalidades;
+
+public class ReprodutorMusical {
+    public void escolherMusica(){
+        System.out.println("Musica Escolhida");
+    }
+    public void escolherAlbum(){
+        System.out.println("Album Escolhida");
+    }
+    public void avaliarAlbum(){
+        System.out.println("Album avaliado em 5 estrelas");
+    }
+    public void pararMusica(){
+        System.out.println("Musica em pausa");
+    }
+    public void reproduzirMusica(){
+        System.out.println("Musica reproduzindo");
+    }
+    public void passarMusica(){
+        System.out.println("Musica passada");
+    }
+    public void voltarMusica(){
+        System.out.println("Voltando para a musica anterior");
+    }
+    public void aumentarVolume(){
+        System.out.println("Aumentando volume");
+    }
+    public void diminuirVolume(){
+        System.out.println("Diminuindo volume");
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/Iphone.java
@@ -0,0 +1,24 @@
+import Funcionalidades.ReprodutorMusical;
+
+
+public class Iphone {
+    public static void main(String[] args) throws Exception {
+       ReprodutorMusical itunes = new ReprodutorMusical();
+       itunes.escolherAlbum();
+       itunes.escolherMusica();
+       itunes.reproduzirMusica();
+       itunes.aumentarVolume();
+       itunes.diminuirVolume();
+       itunes.avaliarAlbum();
+       itunes.pararMusica();
+       itunes.passarMusica();
+       itunes.voltarMusica();
+
+
+
+
+
+    }
+
+
+}
